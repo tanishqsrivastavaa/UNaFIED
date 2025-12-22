010f41f7731ce63a96b2c2865fa4aa633ff803c3
Tanishq Srivastava <tanishq280205@gmail.com>
2025-12-22 12:10:03 +0530

    Add User model with fields for id, email, hashed_password, and created_at

---
+++ backend/app/models/user.py
@@ -0,0 +1,10 @@
+import uuid
+from datetime import datetime
+from sqlmodel import SQLModel, Field
+
+class User(SQLModel,table=True):
+    id: uuid.UUID = Field(default_factory=uuid.uuid4,primary_key=True)
+    email: str = Field(unique=True, index= True)
+    hashed_password: str
+    created_at: datetime = Field(default_factory=datetime.now)
+    
