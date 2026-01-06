361941446f67537dfdc25cddac7cdb765208fa6e
Tanishq Srivastava <tanishq280205@gmail.com>
2026-01-06 09:26:46 +0530

    Implement user authentication retrieval and improve error handling in get_current_user function

--- backend/app/core/security.py
+++ backend/app/core/security.py
@@ -43,3 +43,32 @@ def verify_token(token: str) -> dict[str,Any]:
         return payload
     except PyJWTError:
         raise HTTPException(status_code=401,detail= "Could not validate credentials")
+    
+
+def get_current_user(token:str = Depends(bearer_scheme),session : Session = Depends(get_session)):
+    try:
+        token_string = token.credentials
+
+        payload = jwt.decode(token_string,SECRET_KEY,algorithms=[ALGORITHM])
+        user_id = payload.get("sub")
+
+        if user_id is None:
+            raise HTTPException(
+                status_code = status.HTTP_401_UNAUTHORIZED,
+                detail = "Invalid authentication credentials"
+            ,)
+
+    #querying the db to get user object
+        user = session.exec(select(Users).where(Users.id == user_id)).first()
+        if user is None:
+                raise HTTPException(
+                    status_code = status.HTTP_401_UNAUTHORIZED,
+                    detail = "User not found"
+                ,)
+
+        return user
+    except PyJWTError:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Could not validate credentials",
+        )
