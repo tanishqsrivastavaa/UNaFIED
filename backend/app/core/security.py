ba33b14e4c7234ee36da80c0d797f322f1470158
Tanishq Srivastava <tanishq280205@gmail.com>
2026-01-07 12:19:11 +0530

    refactor: Improve token handling and user authentication in security module

--- backend/app/core/security.py
+++ backend/app/core/security.py
@@ -1,40 +1,53 @@
-##User login and authentication
+from fastapi import Depends,HTTPException
+from sqlmodel import Session
 import os
 from dotenv import load_dotenv
-from typing import Optional
-from datetime import datetime,timedelta
 import jwt
 from jwt import PyJWTError
+from datetime import datetime,timedelta
 from fastapi.security import HTTPBearer,HTTPAuthorizationCredentials
-from fastapi import HTTPException,Depends
-from sqlmodel import Session
-from db.db import get_session
-from typing import Any
 from passlib.context import CryptContext
+from typing import Any
+from db.db import get_session
+
 load_dotenv()
 
-SECRET_KEY= os.getenv("AUTH_KEY")
-if not SECRET_KEY:
-    raise ValueError("No AUTH_KEY found in environment variables!")
+SECRET_KEY= os.getenv("SECRET_AUTH_KEY")
 
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
+#first we decide what algorithm we are going to use
+ALGORITHM= "HS256"
 
-bearer_scheme= HTTPBearer()
+#now we decide how long the token is alive for
+ACCESS_TOKEN_TIME_LIMIT= 15 #15 minutes
 
-def create_access_token(data: dict, expires_delta: timedelta | None=None) -> str:
+bearer_scheme = HTTPBearer()
+
+
+#first we will work on hashing the password
+
+pwd_content = CryptContext(schemes=["bcrypt"],deprecated="auto")
+def get_password_hash(password: str) -> str:
+    return pwd_content.hash(password)
+
+#now we will verify the hashed password
+def verify_password_hash(plain_password: str,hash_password: str) -> bool:
+    return pwd_content.verify(plain_password,hash_password)
+
+
+"""now lets work on the token creation, verification, and getting current user"""
+
+def create_token(data: dict, expires_detla: timedelta | None=None) -> str:
     
     to_encode = data.copy()
 
-    if expires_delta:
-        expire = datetime.now() + expires_delta
+    if expires_detla:
+        expire = datetime.now() + expires_detla
     else:
-        expire = datetime.now() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+        expire = datetime.now() + timedelta(minutes=ACCESS_TOKEN_TIME_LIMIT)
 
     to_encode.update({"exp":expire})
 
-    encoded_jwt = jwt.encode(to_encode,SECRET_KEY,algorithm=ALGORITHM)
-
+    encoded_jwt = jwt.encode(to_encode,SECRET_KEY,ALGORITHM)
     return encoded_jwt
 
 
@@ -43,42 +56,30 @@ def verify_token(token: str) -> dict[str,Any]:
         payload = jwt.decode(token,SECRET_KEY,algorithms=[ALGORITHM])
         return payload
     except PyJWTError:
-        raise HTTPException(status_code=401,detail= "Could not validate credentials")
+        raise HTTPException(status_code=401,detail="Could not validate credentials")
     
+#now we make the function to get current user
 
-def get_current_user(token:HTTPAuthorizationCredentials = Depends(bearer_scheme),session : Session = Depends(get_session)):
+
+def get_current_user(token: HTTPAuthorizationCredentials = Depends(bearer_scheme),session: Session =Depends(get_session)):
+
     try:
         token_string = token.credentials
-
         payload = jwt.decode(token_string,SECRET_KEY,algorithms=[ALGORITHM])
         user_id = payload.get("sub")
-
+        
         if user_id is None:
-            raise HTTPException(
-                status_code = 401,
-                detail = "Invalid authentication credentials"
-            ,)
-
-    #querying the db to get user object
-        user = session.exec(select(Users).where(Users.id == user_id)).first()
+            raise HTTPException(status_code=401,detail="Invalid authentication credentials")
+        
+        user = session.exec(select(Users).where(Users.id==user_id)).first()
         if user is None:
-                raise HTTPException(
-                    status_code = 401,
-                    detail = "User not found"
-                ,)
-
+            raise HTTPException(
+                status_code=401,
+                detail="User not found"
+            )
         return user
     except PyJWTError:
         raise HTTPException(
             status_code=401,
-            detail="Could not validate credentials",
+            detail="Could not validate credentials"
         )
-
-
-pwd_content = CryptContext(schemes=["bcrypt"],deprecated="auto")
-
-def get_password_hash(password: str) -> str:
-    return pwd_content.hash(password)
-
-def verify_password(plain_password: str, hashed_password: str) -> bool:
-    return pwd_content.verify(plain_password,hashed_password)
