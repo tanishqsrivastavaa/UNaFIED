15c979d40ea1a7c57bf970f91da967fcd554974f
Tanishq Srivastava <tanishq280205@gmail.com>
2026-01-08 11:01:04 +0530

    refactor: Clean up user module by removing redundant model definitions and organizing imports

--- backend/app/api/v1/users.py
+++ backend/app/api/v1/users.py
@@ -1,20 +1,12 @@
-from ...core.security import get_password_hash,verify_password_hash,create_token
+from ...core.security import get_password_hash,verify_password_hash,create_token,get_current_user
 from fastapi import APIRouter, HTTPException, Depends
-from fastapi.security import HTTPBearer
+from fastapi.security import HTTPBearer,OAuth2PasswordRequestForm
 from sqlmodel import select,Session
 from ...db.db import get_session
 from ...models.user import User
-from pydantic import BaseModel
+from ...schemas.user import LoginRequest, UserCreate
 
 router = APIRouter(tags=["users"])
-
-class UserCreate(BaseModel):
-    email: str
-    password: str
-
-class LoginRequest(BaseModel):
-    email: str
-    password: str
 
 @router.post("/signup",response_model=User)
 def signup(user_data: UserCreate, session: Session= Depends(get_session)):
@@ -45,3 +37,6 @@ def login(body: LoginRequest, session: Session= Depends(get_session)):
     token = create_token({"sub":str(user.id)})
     return {"access_token" : token,"token_type":"Bearer"}
 
+@router.get("/me")
+async def read_users_me(current_user:User = Depends(get_current_user)):
+    return current_user
