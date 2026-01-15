516209b10a686ee1a709966fcecfdf141c568f5d
Tanishq Srivastava <tanishq280205@gmail.com>
2026-01-15 10:16:56 +0530

    feat: Add SuggestedAction and AgentResponse models for enhanced chat functionality

--- backend/app/agents/chat_agent.py
+++ backend/app/agents/chat_agent.py
@@ -1,7 +1,10 @@
 from pathlib import Path
+from typing import Optional
+from pydantic import BaseModel,Field
 from pydantic_ai import Agent
 from pydantic_ai.models.groq import GroqModel
 from pydantic_ai.providers.groq import GroqProvider
+from ..core.tools import get_weather
 from ..core.tools import get_weather
 from dotenv import load_dotenv
 import os
@@ -15,11 +18,29 @@ GROQ_API_KEY= os.getenv("GROQ_API_KEY")
 prompt= Path("app/prompts/prompt.txt").read_text(encoding="utf-8")
 
 
+class SuggestedAction(BaseModel):
+    label: str= Field(description="Short text for the button, e.g., 'Check Weather'")
+    tool_name: str= Field(description="The name to call the cool")
+    parameters: dict= Field(description="The parameters passed to the tool")
+
+
+
+class AgentResponse(BaseModel):
+    chat_message: str = Field(description="The natural language reply to the user's input.")
+    suggestion: Optional[SuggestedAction]= Field(
+        default=None, 
+        description="Optional. Only provide this if you are proposing a concrete action for the user to approve."
+    )
+
+
+
 chat_agent= Agent(
     GroqModel(
         model_name="llama-3.3-70b-versatile",
         provider=GroqProvider(api_key=GROQ_API_KEY)),
         system_prompt=prompt,
+        result_type=AgentResponse,
         tools=[get_weather],
         deps_type=str
         )
+
