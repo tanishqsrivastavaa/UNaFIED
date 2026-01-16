ebefdd8509676bd5a62a737bfce6becb3a32c20e
Tanishq Srivastava <tanishq280205@gmail.com>
2026-01-16 20:29:13 +0530

    refactor: Remove commented-out process_chat_message method for cleaner code

--- backend/app/services/chat.py
+++ backend/app/services/chat.py
@@ -55,49 +55,6 @@ class ChatService:
         )
         return session.exec(statement=statement).first()
     
-    # @staticmethod
-    # async def process_chat_message(
-    #     session: Session,
-    #     conversation_id: uuid.UUID,
-    #     user_id: uuid.UUID,
-    #     message_in: MessageCreate
-    # ) -> Message:
-        
-
-    #     statement= select(Conversation).where(
-    #         Conversation.id==conversation_id,
-    #         Conversation.user_id==user_id
-    #     )
-
-    #     conversation= session.exec(statement).first()
-
-    #     if not conversation:
-    #         raise ValueError("Conversation not found or access denied!")
-        
-    #     user_message= Message(
-    #         conversation_id=conversation_id,
-    #         role="user",
-    #         content= message_in.content
-    #     )
-
-    #     session.add(user_message)
-    #     session.commit()
-    #     session.refresh(user_message)
-
-    #     result= chat_agent.run_sync(message_in.content,deps=message_in.content)
-
-    #     assistant_message= Message(
-    #         conversation_id=conversation_id,
-    #         role="assistant",
-    #         content=result.output
-    #     )
-
-    #     session.add(assistant_message)
-    #     session.commit()
-    #     session.refresh(assistant_message)
-
-    #     return assistant_message
-    
     @staticmethod
     def get_chat_history(session:Session, conversation_id:uuid.UUID) -> list[ModelMessage]:
 
