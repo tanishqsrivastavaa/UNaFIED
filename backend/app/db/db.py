16672b04d19d0cd9042a6c0387081f5293809763
Tanishq Srivastava <tanishq280205@gmail.com>
2026-02-22 11:54:21 +0530

    feat: Implement new agent tools, add comprehensive authentication and tool testing, and introduce Docker Compose for environment setup.

--- backend/app/db/db.py
+++ backend/app/db/db.py
@@ -1,13 +1,17 @@
-from sqlmodel import Session,SQLModel,create_engine
 import os
+from sqlmodel import Session, SQLModel, create_engine
 from dotenv import load_dotenv
+
 load_dotenv()
 
-DATABASE_URL= os.getenv("DATABASE_URL")
+DATABASE_URL = os.getenv("DATABASE_URL")
 if not DATABASE_URL:
     raise ValueError("DB URL NOT PRESENT")
 
-engine = create_engine(url=DATABASE_URL,echo=True)
+DEBUG = os.getenv("DEBUG", "false").lower() == "true"
+
+engine = create_engine(url=DATABASE_URL, echo=DEBUG)
+
 
 def get_session():
     with Session(engine) as session:
